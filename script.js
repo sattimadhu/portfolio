20793de453cefe2292d4ef057d356540070c746d
sattimadhu <madhusatti2007@gmail.com>
2024-12-21 21:42:02 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav ul li a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav ul li a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+};
